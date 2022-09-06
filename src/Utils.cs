557aecb79cd0c0284f6d2b6121e98dbc19d69dc2
Nullpointer <mikeschulze.lpz@kabelmail.de>
2022-09-06 15:53:23 +0200

    GD-14: Complete GdUnit utils (#22)

--- src/Utils.cs
+++ src/Utils.cs
@@ -3,12 +3,66 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace GdUnit3
 {
 
     public sealed class Utils
     {
+
+        static Dictionary<Godot.Error, string> GODOT_ERRORS = new Dictionary<Godot.Error, string>(){
+            {Godot.Error.Failed, "Generic error."},
+            {Godot.Error.Unavailable, "Unavailable error."},
+            {Godot.Error.Unconfigured, "Unconfigured error."},
+            {Godot.Error.Unauthorized, "Unauthorized error."},
+            {Godot.Error.ParameterRangeError, "Parameter range error."},
+            {Godot.Error.OutOfMemory, "Out of memory (OOM) error."},
+            {Godot.Error.FileNotFound,"File: Not found error."},
+            {Godot.Error.FileBadDrive, "File: Bad drive error."},
+            {Godot.Error.FileBadPath, "File: Bad path error."},
+            {Godot.Error.FileNoPermission, "File: No permission error."},
+            {Godot.Error.FileAlreadyInUse, "File: Already in use error."},
+            {Godot.Error.FileCantOpen,"File: Can't open error."},
+            {Godot.Error.FileCantWrite, "File: Can't write error."},
+            {Godot.Error.FileCantRead, "File: Can't read error."},
+            {Godot.Error.FileUnrecognized, "File: Unrecognized error."},
+            {Godot.Error.FileCorrupt, "File: Corrupt error."},
+            {Godot.Error.FileMissingDependencies, "File: Missing dependencies error."},
+            {Godot.Error.FileEof, "File: End of file (EOF) error."},
+            {Godot.Error.CantOpen, "Can't open error."},
+            {Godot.Error.CantCreate,"Can't create error."},
+            {Godot.Error.QueryFailed, "Query failed error."},
+            {Godot.Error.AlreadyInUse, "Already in use error."},
+            {Godot.Error.Locked, "Locked error."},
+            {Godot.Error.Timeout, "Timeout error."},
+            {Godot.Error.CantConnect, "Can't connect error."},
+            {Godot.Error.CantResolve, "Can't resolve error."},
+            {Godot.Error.ConnectionError, "Connection error."},
+            {Godot.Error.CantAcquireResource, "Can't acquire resource error."},
+            {Godot.Error.CantFork, "Can't fork process error."},
+            {Godot.Error.InvalidData, "Invalid data error."},
+            {Godot.Error.InvalidParameter, "Invalid parameter error."},
+            {Godot.Error.AlreadyExists, "Already exists error."},
+            {Godot.Error.DoesNotExist, "Does not exist error."},
+            {Godot.Error.DatabaseCantRead, "Database: Read error."},
+            {Godot.Error.DatabaseCantWrite, "Database: Write error."},
+            {Godot.Error.CompilationFailed, "Compilation failed error."},
+            {Godot.Error.MethodNotFound, "Method not found error."},
+            {Godot.Error.LinkFailed, "Linking failed error."},
+            {Godot.Error.ScriptFailed, "Script failed error."},
+            {Godot.Error.CyclicLink, "Cycling link (import cycle) error."},
+            {Godot.Error.InvalidDeclaration, "Invalid declaration error."},
+            {Godot.Error.DuplicateSymbol, " Duplicate symbol error."},
+            {Godot.Error.ParseError, "Parse error."},
+            {Godot.Error.Busy, "Busy error."},
+            {Godot.Error.Skip, "Skip error."},
+            {Godot.Error.Help, "Help error."},
+            {Godot.Error.Bug, "Bug error."},
+            {Godot.Error.PrinterOnFire, "Printer on fire error. (This is an easter egg, no engine methods return this error code.)"}
+        };
+
+
         public async static Task<long> DoWait(long timeout)
         {
             System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
@@ -51,5 +105,14 @@ namespace GdUnit3
             if (Directory.Exists(tempFolder))
                 Directory.Delete(tempFolder, true);
         }
+
+        /// <summary>
+        /// Mapps Godot error number to a readable error message. See at ERROR
+        /// https://docs.godotengine.org/de/stable/classes/class_@globalscope.html#enum-globalscope-error
+        /// </summary>
+        /// <param name="errorNumber"></param>
+        /// <returns></returns>
+        public static string ErrorAsString(Godot.Error errorNumber) => GODOT_ERRORS.GetValueOrDefault(errorNumber, $"The error: {errorNumber} is not defined in Godot.");
+        public static string ErrorAsString(int errorNumber) => ErrorAsString((Godot.Error)Enum.ToObject(typeof(Godot.Error), errorNumber));
     }
 }
--- test/UtilsTest.cs
+++ test/UtilsTest.cs
@@ -3,47 +3,58 @@ using System.IO;
 
 namespace GdUnit3.Tests
 {
-	using static Utils;
-	using static Assertions;
+    using static Utils;
+    using static Assertions;
 
-	[TestSuite]
-	public class UtilsTest
-	{
-		[TestCase]
-		public void CreateTempDir_success()
-		{
-			string tempDir = CreateTempDir("foo");
-			AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "foo"));
-			AssertThat(Directory.Exists(tempDir)).IsTrue();
+    [TestSuite]
+    public class UtilsTest
+    {
+        [TestCase]
+        public void CreateTempDir_success()
+        {
+            string tempDir = CreateTempDir("foo");
+            AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "foo"));
+            AssertThat(Directory.Exists(tempDir)).IsTrue();
 
-			tempDir = CreateTempDir("bar1\\test\\foo");
-			AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "bar1\\test\\foo"));
-			AssertThat(Directory.Exists(tempDir)).IsTrue();
+            tempDir = CreateTempDir("bar1\\test\\foo");
+            AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "bar1\\test\\foo"));
+            AssertThat(Directory.Exists(tempDir)).IsTrue();
 
-			tempDir = CreateTempDir("bar2/test/foo");
-			AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "bar2/test/foo"));
-			AssertThat(Directory.Exists(tempDir)).IsTrue();
-		}
+            tempDir = CreateTempDir("bar2/test/foo");
+            AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "bar2/test/foo"));
+            AssertThat(Directory.Exists(tempDir)).IsTrue();
+        }
 
-		[TestCase]
-		public void CreateTempDir_atTwice()
-		{
-			string tempDir = CreateTempDir("foo");
-			// create again
-			CreateTempDir("foo");
-			
-			AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "foo"));
-			AssertThat(Directory.Exists(tempDir)).IsTrue();
-		}
+        [TestCase]
+        public void CreateTempDir_atTwice()
+        {
+            string tempDir = CreateTempDir("foo");
+            // create again
+            CreateTempDir("foo");
 
-		[TestCase]
-		public void ClearTempDir_success()
-		{
-			string tempDir = CreateTempDir("foo");
-			AssertThat(Directory.Exists(tempDir)).IsTrue();
+            AssertThat(tempDir).IsEqual(Path.Combine(GodotTempDir(), "foo"));
+            AssertThat(Directory.Exists(tempDir)).IsTrue();
+        }
 
-			ClearTempDir();
-			AssertThat(Directory.Exists(tempDir)).IsFalse();
-		}
-	}
+        [TestCase]
+        public void ClearTempDir_success()
+        {
+            string tempDir = CreateTempDir("foo");
+            AssertThat(Directory.Exists(tempDir)).IsTrue();
+
+            ClearTempDir();
+            AssertThat(Directory.Exists(tempDir)).IsFalse();
+        }
+
+        [TestCase]
+        public void GodotErrorAsString()
+        {
+            AssertThat(ErrorAsString(Godot.Error.Bug)).IsEqual("Bug error.");
+            AssertThat(ErrorAsString(47)).IsEqual("Bug error.");
+
+            // with not existing error number
+            AssertThat(ErrorAsString(100)).IsEqual("The error: 100 is not defined in Godot.");
+        }
+
+    }
 }
