47cc78e4c2fdbd57afac207418f99b897ce86fee
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2023-05-31 20:21:17 +0200

    append

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
 using System;
 
-class Program
+public partial class Program
 {
-    
-        static void Main(string[] args)
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+    }
+
+
+
+    public partial class SignalCollectorTest : Godot.GodotObject
+    {
+        public void ConnectAllSignals(Godot.GodotObject emitter)
         {
-            Console.WriteLine("Hello World");
+            foreach (Godot.Collections.Dictionary signalDef in emitter.GetSignalList())
+            {
+                string signalName = (string)signalDef["name"];
+
+                var cb = new Godot.Callable(this, nameof(OnSignalEmmited));
+                //cb.Bind({ emitter, signalName });
+                if (!emitter.IsConnected(signalName, cb))
+                {
+                    emitter.Connect(signalName, cb);
+                }
+            }
         }
+
+        private void OnSignalEmmited(Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName);
+        private void OnSignalEmmited(object arg1, Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName, new[] { arg1 });
+        private void OnSignalEmmited(object arg1, object arg2, Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName, new[] { arg1, arg2 });
+    }
 }
--- TestRunner.cs
+++ TestRunner.cs
@@ -10,36 +10,6 @@ using Godot;
 
 namespace GdUnit4
 {
-
-    public partial class SignalCollectorTest : Godot.GodotObject
-    {
-
-
-
-        public void ConnectAllSignals(Godot.GodotObject emitter)
-        {
-
-   
-
-            foreach (Godot.Collections.Dictionary signalDef in emitter.GetSignalList())
-            {
-                string signalName = (string)signalDef["name"];
-
-                var cb = new Godot.Callable(this, nameof(OnSignalEmmited));
-                //cb.Bind({ emitter, signalName });
-                if (!emitter.IsConnected(signalName, cb))
-                {
-                    emitter.Connect(signalName, cb);
-                }
-            }
-        }
-
-        private void OnSignalEmmited(Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName);
-        private void OnSignalEmmited(object arg1, Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName, new[] { arg1 });
-        private void OnSignalEmmited(object arg1, object arg2, Godot.GodotObject emitter, string signalName) => Godot.GD.PrintS(emitter, signalName, new[] { arg1, arg2 });
-    }
-
-
     class TestReporter : ITestEventListener
     {
 
