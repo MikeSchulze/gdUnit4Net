c2126f9406a6d7892c9c37135d79260d5b8ebdeb
Nullpointer <mikeschulze.lpz@kabelmail.de>
2024-02-24 09:53:22 +0100

    append

--- clean.sh
+++ clean.sh
@@ -1,17 +1,27 @@
 rm -rf ./bin
 rm -rf ./obj
-rm -rf ./api/bin
+rm -rf ./api/nupkg/net7.0/
+rm -rf ./api/nupkg/*.nupkg
 rm -rf ./api/obj
-rm -rf ./test/.godot
-rm -rf ./testadapter/.godot
-rm -rf ./testadapter/bin
-rm -rf ./testadapter/obj
-rm -rf ./testadapter/obj
+
 rm -rf ./test/.godot
 rm -rf ./test/gdunit4_testadapter
+
 rm -rf ./example/.godot
 rm -rf ./example/gdunit4_testadapter
 
-dotnet clean
+
+rm -rf ./testadapter/nupkg/net7.0/
+rm -rf ./testadapter/nupkg/*.nupkg
+rm -rf ./testadapter/obj
+
+
+dotnet build ./api/gdUnit4Api.csproj
+dotnet build ./testadapter/gdUnit4TestAdapter.csproj
 dotnet restore
 dotnet build
+
+# dotnet clean
+# dotnet restore
+# dotnet build
+# "dotnet.unitTests.runSettingsPath": "./test/.runsettings"
