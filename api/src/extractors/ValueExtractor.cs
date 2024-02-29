4b099fc94341ae2e80ba0f38313c254e00ec687b
Nullpointer <mikeschulze.lpz@kabelmail.de>
2024-02-29 21:49:32 +0100

    fix formattings
    /src/extractors

--- api/src/extractors/ValueExtractor.cs
+++ api/src/extractors/ValueExtractor.cs
@@ -1,62 +1,61 @@
+namespace GdUnit4.Asserts;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace GdUnit4.Asserts
+public sealed class ValueExtractor : IValueExtractor
 {
-    public sealed class ValueExtractor : IValueExtractor
+    private readonly IEnumerable<string> methodNames;
+
+    private readonly IEnumerable<object> args;
+
+    public ValueExtractor(string methodName, params object[] args)
     {
-        private readonly IEnumerable<string> _methodNames;
+        methodNames = methodName.Split('.');
+        this.args = args.ToList();
+    }
 
-        private readonly IEnumerable<object> _args;
+    public object? ExtractValue(object? value)
+    {
+        if (value == null)
+            return null;
 
-        public ValueExtractor(string methodName, params object[] args)
+        foreach (var methodName in methodNames)
         {
-            _methodNames = methodName.Split('.');
-            _args = args.ToList<object>();
-        }
-
-        public object? ExtractValue(object? value)
-        {
-            if (value == null)
-                return null;
-
-            foreach (var methodName in _methodNames)
+            try
             {
-                try
-                {
-                    value = Extract(value, methodName).UnboxVariant();
-                    if (value == null || value.Equals("n.a."))
-                        return value;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($"Can't ExtractValue {methodName}:{value}\n {e.StackTrace}");
-                    return "n.a.";
-                }
+                value = Extract(value, methodName).UnboxVariant();
+                if (value == null || value.Equals("n.a."))
+                    return value;
             }
-            return value;
-        }
-
-        private object? Extract(object instance, string name)
-        {
-            var type = instance.GetType();
-            var method = type.GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            if (method != null)
+            catch (Exception e)
             {
-                if (_args.Count() > 0 && instance is Godot.GodotObject go)
-                    return go.Callv(method.Name.ToSnakeCase(), _args.ToGodotArray()).UnboxVariant();
-                return method.Invoke(instance, _args.ToArray());
-            }
-            var property = type.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
-            if (property == null)
-            {
-                //if GdUnitSettings.is_verbose_assert_warnings():
-                //    Godot.GD.PushWarning("Extracting value from element '%s' by func '%s' failed! Converting to \"n.a.\"" % [instance, func_name])
+                Console.WriteLine($"Can't ExtractValue {methodName}:{value}\n {e.StackTrace}");
                 return "n.a.";
             }
-            return property.GetValue(instance);
         }
+        return value;
+    }
+
+    private object? Extract(object instance, string name)
+    {
+        var type = instance.GetType();
+        var method = type.GetMethod(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        if (method != null)
+        {
+            if (args.Any() && instance is Godot.GodotObject go)
+                return go.Callv(method.Name.ToSnakeCase(), args.ToGodotArray()).UnboxVariant();
+            return method.Invoke(instance, args.ToArray());
+        }
+        var property = type.GetProperty(name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        if (property == null)
+        {
+            //if GdUnitSettings.is_verbose_assert_warnings():
+            //    Godot.GD.PushWarning("Extracting value from element '%s' by func '%s' failed! Converting to \"n.a.\"" % [instance, func_name])
+            return "n.a.";
+        }
+        return property.GetValue(instance);
     }
 }
