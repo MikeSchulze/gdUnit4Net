d6990635e9dd57a7be56393842ba22056aab6f5b
Nullpointer <mikeschulze.lpz@gmail.com>
2025-07-04 17:34:37 +0200

    GD-288: Fix invalid delay value on calling `_Processs` when using `AssertSignal`
    # Why
    When testing signals by `AssertSignal` and the emitter has overwritten the `_Processs` the delta parameter has a value of 10s.
    
    # What
    - Fixing the delta to correct value of 10ms
    - Fixing the evaluation of manuall calling  `_Process` and `_PhysicsProcess` methods

---
+++ Api.Test/src/core/signals/GodotSignalCollectorTest.cs
@@ -0,0 +1,98 @@
+﻿namespace GdUnit4.Tests.Core.signals;
+
+using GdUnit4.Core.Signals;
+
+using Godot;
+
+using static Assertions;
+
+[TestSuite]
+[RequireGodotRuntime]
+public partial class GodotSignalCollectorTest
+{
+    [TestCase]
+    public void DoesNodeProcessingOnParentLessNode()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new Node())!;
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The node is not hooked into the scene tree, and the `_Process` and `_PhysicsProcess` are not overwritten
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsFalse();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsFalse();
+    }
+
+    [TestCase]
+    public void DoesNodeProcessingOnParentLessObject()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new GodotObject())!;
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The object is not hooked into the scene tree, and the `_Process` and `_PhysicsProcess` are not overwritten
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsFalse();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsFalse();
+    }
+
+    [TestCase]
+    public void DoesNodeProcessingOnParentNode()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new Node())!;
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var parent = AutoFree(new Node())!;
+        parent.AddChild(emitter);
+
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The node is not hooked into the scene tree, and the `_Process` and `_PhysicsProcess` are not overwritten
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsFalse();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsFalse();
+    }
+
+    [TestCase]
+    public void DoesNodeProcessingOnEngineAddedNode()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new Node())!;
+        AddNode(emitter);
+
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The node is hooked into the scene tree, we don't need to call manually the `_Process` and `_PhysicsProcess`
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsFalse();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsFalse();
+    }
+
+    [TestCase]
+    public void DoesNodeProcessingOnEngineAddedNodeImplements_Process()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new EmitterWithProcessHandler())!;
+        AddNode(emitter);
+
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The node is hooked into the scene tree, we don't need to call manually the `_Process` and `_PhysicsProcess`
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsFalse();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsFalse();
+    }
+
+    [TestCase]
+    public void DoesNodeProcessingOnEngineNodeImplements_Process()
+    {
+        // ReSharper disable once NullableWarningSuppressionIsUsed
+        var emitter = AutoFree(new EmitterWithProcessHandler())!;
+
+        var doesNodeProcessing = GodotSignalCollector.DoesNodeProcessing(emitter);
+        // The node is not hooked into the scene tree, we must call manually the `_Process` and `_PhysicsProcess`
+        AssertThat(doesNodeProcessing.NeedsCallProcessing).IsTrue();
+        AssertThat(doesNodeProcessing.NeedsCallPhysicsProcessing).IsTrue();
+    }
+
+    public partial class EmitterWithProcessHandler : Node
+    {
+        public override void _Process(double delta)
+        {
+        }
+
+        public override void _PhysicsProcess(double delta)
+        {
+        }
+    }
+}
