3fab1d553f607ef775e9c1e5f22efc7331555c96
Caleb Brinkman <floralvikings@gmail.com>
2026-03-21 13:38:01 -0500

    Update IParameterResolver signatures to use IParameterContext

--- Api/src/api/IParameterResolver.cs
+++ Api/src/api/IParameterResolver.cs
@@ -2,8 +2,6 @@
 // MIT License - See LICENSE file in the repository root for full license text
 namespace GdUnit4.Api;
 
-using System.Reflection;
-
 /// <summary>
 /// Interface for test extensions that support parameter resolution, allowing them to provide values for parameters in test methods.
 /// </summary>
@@ -13,17 +11,17 @@ public interface IParameterResolver : ITestExtension
     /// Returns whether this extension supports resolving the given parameter.
     /// If true, <see cref="ResolveParameter"/> will be called to obtain the value for the parameter.
     /// </summary>
-    /// <param name="parameter">The parameter to check for resolution eligibility.</param>
-    /// <param name="context">The extension context providing information about the test suite and execution.</param>
+    /// <param name="parameterContext">The parameter context to check for resolution eligibility.</param>
+    /// <param name="executionContext">The extension context providing information about the test suite and execution.</param>
     /// <returns>True if the parameter is supported.</returns>
-    bool SupportsParameter(ParameterInfo parameter, IExtensionContext context);
+    bool SupportsParameter(IParameterContext parameterContext, IExtensionContext executionContext);
 
     /// <summary>
     /// Resolves the value for the given parameter. This method is called only if <see cref="SupportsParameter"/>
     /// returns true for the parameter.
     /// </summary>
-    /// <param name="parameter">The parameter to resolve.</param>
-    /// <param name="context">The extension context providing information about the test suite and execution.</param>
+    /// <param name="parameterContext">The parameter context to resolve into an instance.</param>
+    /// <param name="executionContext">The extension context providing information about the test suite and execution.</param>
     /// <returns>The value to be passed into the parameter.</returns>
-    object? ResolveParameter(ParameterInfo parameter, IExtensionContext context);
+    object? ResolveParameter(IParameterContext parameterContext, IExtensionContext executionContext);
 }
