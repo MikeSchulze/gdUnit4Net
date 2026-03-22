a992e3532379f1a4215d0c18879fe95fb7e693b1
Nullpointer <mikeschulze.lpz@gmail.com>
2026-03-22 09:10:39 +0100

    Apply suggestions from code review
    Co-authored-by: Nullpointer <mikeschulze.lpz@gmail.com>

--- Api/src/api/IParameterStore.cs
+++ Api/src/api/IParameterStore.cs
@@ -14,7 +14,7 @@ public interface IParameterStore
     /// <typeparam name="T">The type of object to store.</typeparam>
     /// <param name="key">The key to use when retrieving the value.</param>
     /// <param name="value">The value to store.</param>
-    void Put<T>(string key, T value)
+    void Add<T>(string key, T value)
         where T : notnull;
 
     /// <summary>
@@ -23,19 +23,19 @@ public interface IParameterStore
     /// <typeparam name="T">The expected type of object to retrieve.</typeparam>
     /// <param name="key">The key used when the value was stored.</param>
     /// <returns>The value stored at the specified key, or null if there is no value stored at the key.</returns>
-    T? Get<T>(string key);
+    T? Value<T>(string key);
 
     /// <summary>
-    /// Delete the value stored at the specified key and return it.
+    /// Removes the value stored at the specified key and return it.
     /// If there is no value stored at the key, returns null.
-    /// Does **not** cascade to suite context like <see cref="Get{T}"/>. This means that if there is not a value stored
+    /// Does **not** cascade to suite context like <see cref="Value{T}"/>. This means that if there is not a value stored
     /// at the specified key in a test case context, this method will return null even if there is a value stored at the
     /// key in the suite context. This ensures that test-level callbacks cannot accidentally delete suite-level values.
     /// </summary>
     /// <param name="key">The key specifying the value to be deleted.</param>
     /// <typeparam name="T">The expected type of value stored at the specified key.</typeparam>
     /// <returns>The deleted value, if it exists; null otherwise.</returns>
-    T? Erase<T>(string key);
+    T? Remove<T>(string key);
 
     /// <summary>
     /// Gets the number of entries in the internal data store for this context.
