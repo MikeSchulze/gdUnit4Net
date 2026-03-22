daf200a1684c80e555034c0de50e9aae26c59aca
Caleb Brinkman <floralvikings@gmail.com>
2026-03-22 18:53:31 -0500

    GD-400: Implement and test `IExtensionContext`
    # Why
    
    In order to be usable, `IExtensionContext` needs a concrete
    implementation.
    
    # What
    
    Includes an implementation of `IExtensionContext` that optionally
    accepts a "parent" context used to populate the context's data store's
    parent.

---
+++ Api.Test/src/core/testExtensions/ExtensionContextTest.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections.Generic;
+using GdUnit4.Api;
+using GdUnit4.Core.TestExtensions;
+using static GdUnit4.Assertions;
+
+namespace GdUnit4.Tests.Core.TestExtensions;
+
+[TestSuite]
+public class ExtensionContextTest
+{
+    private static IExtensionContext MakeContext(IExtensionContext? parentContext = null, string? methodName = null)
+    {
+        if (parentContext == null)
+            return new ExtensionContext(
+                typeof(ExtensionContextTest),
+                new TestSuiteNode
+                {
+                    ManagedType = "",
+                    Tests = [],
+                    AssemblyPath = "",
+                    SourceFile = "",
+                    Id = Guid.NewGuid(),
+                    ParentId = Guid.NewGuid()
+                },
+                null,
+                null,
+                []);
+
+        var method = typeof(ExtensionContextTest).GetMethod(methodName ?? nameof(MakeContext));
+        return new ExtensionContext(
+            parentContext,
+            method,
+            method?.Name,
+            []);
+    }
+
+    [TestCase]
+    [ThrowsException(typeof(InvalidOperationException))]
+    public void TestStorageThrowsWhenNoSuiteOrTestLevelDataFound()
+    {
+        var context = MakeContext();
+        context.GetStore().Value<object>("notARealKey");
+    }
+
+
+    [TestCase]
+    [ThrowsException(typeof(InvalidOperationException))]
+    public void TestStorageThrowsWhenTypeDoesNotMatch()
+    {
+        var context = MakeContext();
+        
+        // Store a string value at the test level
+        context.GetStore().Add("stringKey", "This is a string");
+
+        // Attempt to retrieve it as an int, which should throw
+        context.GetStore().Value<List<string>>("stringKey");
+    }
+
+
+    [TestCase]
+    public void TestStorageUsesParentContextWhenNotFound()
+    {
+        var suiteContext = MakeContext();
+        var testContext = MakeContext(suiteContext, nameof(TestStorageUsesParentContextWhenNotFound));
+        
+        suiteContext.GetStore().Add("suiteKey", "suiteValue");
+        
+        var result = testContext.GetStore().Value<string>("suiteKey");
+        
+        AssertString(result)
+            .IsEqual("suiteValue");
+    }
+
+
+    [TestCase]
+    public void TestStorageUsesTestLevelValueWhenFound()
+    {
+        var suiteContext = MakeContext();
+        var testContext = MakeContext(suiteContext, nameof(TestStorageUsesTestLevelValueWhenFound));
+        
+        suiteContext.GetStore().Add("key", "suite-level value");
+        testContext.GetStore().Add("key", "test-level value");
+
+        AssertThat(suiteContext.GetStore().Value<string>("key"))
+            .IsEqual("suite-level value");
+
+        AssertThat(testContext.GetStore().Value<string>("key"))
+            .IsEqual("test-level value");
+        
+        testContext.GetStore().Remove<string>("key");
+        
+        AssertThat(testContext.GetStore().Value<string>("key"))
+            .IsEqual("suite-level value");
+    }
+
+    [TestCase]
+    public void TestDeleteReturnsStoredValue()
+    {
+        var context = MakeContext();
+        
+        context.GetStore().Add("key", "value");
+        
+        var countAfterAdding = context.GetStore().Count();
+
+        var result = context.GetStore().Remove<string>("key");
+        
+        var countAfterDeleting = context.GetStore().Count();
+
+        AssertString(result)
+            .IsEqual("value");
+
+        AssertInt(countAfterAdding - countAfterDeleting)
+            .IsEqual(1);
+    }
+
+    [TestCase]
+    public void TestDeleteReturnsDefaultWhenKeyNotFound()
+    {
+        var context = MakeContext();
+        
+        var result = context.GetStore().Remove<string>("nonexistent");
+
+        AssertObject(result)
+            .IsNull();
+    }
+
+    [TestCase]
+    [ThrowsException(typeof(InvalidOperationException))]
+    public void TestRemoveCascadesToSuiteLevel()
+    {
+        // Populate suite-level storage by using a suite-level context (TestCaseName == null)
+        var suiteContext = MakeContext();
+        suiteContext.GetStore().Add("shared", "suite-value");
+
+        // Test-level context initialised from suite data
+        var testContext = MakeContext(suiteContext, nameof(TestRemoveCascadesToSuiteLevel));
+
+        // Remove at test level
+        testContext.GetStore().Remove<string>("shared");
+
+        // This should throw as the parent entry will have been removed
+        testContext.GetStore().Value<string>("shared");
+    }
+}
---
+++ Api/src/core/testExtensions/ExtensionContext.cs
@@ -0,0 +1,60 @@
+// Copyright (c) 2025 Mike Schulze
+// MIT License - See LICENSE file in the repository root for full license text
+namespace GdUnit4.Core.TestExtensions;
+
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+using Api;
+
+internal class ExtensionContext : IExtensionContext
+{
+    private readonly Type testSuiteType;
+
+    private readonly TestSuiteNode testSuiteInstance;
+
+    private readonly MethodInfo? testMethod;
+
+    private readonly IParameterStore parameterStore;
+
+    private readonly string? testCaseName;
+
+    private readonly ReadOnlyCollection<object?> testCaseArguments;
+
+    public ExtensionContext(IExtensionContext parentContext, MethodInfo? testMethod, string? testCaseName, List<object?> testCaseArguments)
+    {
+        parameterStore = new ParameterStore(parentContext.GetStore());
+        testSuiteType = parentContext.GetTestSuiteType();
+        testSuiteInstance = parentContext.GetTestSuiteInstance();
+        this.testMethod = testMethod;
+        this.testCaseName = testCaseName;
+        this.testCaseArguments = new ReadOnlyCollection<object?>(testCaseArguments);
+    }
+
+    public ExtensionContext(
+        Type testSuiteType,
+        TestSuiteNode testSuiteInstance,
+        MethodInfo? testMethod,
+        string? testCaseName,
+        List<object?> testCaseArguments)
+    {
+        parameterStore = new ParameterStore();
+        this.testSuiteType = testSuiteType;
+        this.testSuiteInstance = testSuiteInstance;
+        this.testMethod = testMethod;
+        this.testCaseName = testCaseName;
+        this.testCaseArguments = new ReadOnlyCollection<object?>(testCaseArguments);
+    }
+
+    public Type GetTestSuiteType() => testSuiteType;
+
+    public TestSuiteNode GetTestSuiteInstance() => testSuiteInstance;
+
+    public MethodInfo? GetTestMethod() => testMethod;
+
+    public string? GetTestCaseName() => testCaseName;
+
+    public ReadOnlyCollection<object?> GetTestCaseArguments() => testCaseArguments;
+
+    public IParameterStore GetStore() => parameterStore;
+}
