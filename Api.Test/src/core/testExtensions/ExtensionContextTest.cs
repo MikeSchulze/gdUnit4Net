e04fb6897d5e5cb5c3318b1b97d265575eba308f
Caleb Brinkman <floralvikings@gmail.com>
2026-03-26 10:21:52 -0500

    Rename helper methods, merge tests

--- Api.Test/src/core/testExtensions/ExtensionContextTest.cs
+++ Api.Test/src/core/testExtensions/ExtensionContextTest.cs
@@ -24,128 +24,45 @@ public class ExtensionContextTest
         typeof(ExtensionContextTest).GetMethod(name)
             ?? throw new InvalidOperationException($"Method '{name}' not found");
 
-    private static IExtensionContext MakeSuiteContext() =>
+    private static IExtensionContext CreateSuiteContext() =>
         new ExtensionContext(typeof(ExtensionContextTest), SuiteNode);
 
-    private static IExtensionContext MakeTestContext(IExtensionContext parent, string methodName, List<object?> args) =>
+    private static IExtensionContext CreateTestContext(IExtensionContext parent, string methodName, List<object?> args) =>
         new ExtensionContext(parent, GetMethod(methodName), methodName, args);
 
-    #region SuiteLevelConstructor
-
     [TestCase]
-    public void TestSuiteConstructorReturnsCorrectTestSuiteType()
+    public void TestConstructorAtSuiteLevel()
     {
-        var context = MakeSuiteContext();
+        var context = CreateSuiteContext();
         AssertThat(context.GetTestSuiteType()).IsEqual(typeof(ExtensionContextTest));
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorReturnsCorrectTestSuiteInstance()
-    {
-        var context = MakeSuiteContext();
         AssertThat(context.GetTestSuiteInstance()).IsEqual(SuiteNode);
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorReturnsNullTestMethod()
-    {
-        var context = MakeSuiteContext();
         AssertObject(context.GetTestMethod()).IsNull();
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorReturnsNullTestCaseName()
-    {
-        var context = MakeSuiteContext();
         AssertObject(context.GetTestCaseName()).IsNull();
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorReturnsNullTestCaseArguments()
-    {
-        var context = MakeSuiteContext();
         AssertObject(context.GetTestCaseArguments()).IsNull();
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorReturnsNonNullStore()
-    {
-        var context = MakeSuiteContext();
         AssertObject(context.GetStore()).IsNotNull();
-    }
-
-    [TestCase]
-    public void TestSuiteConstructorStoreIsInitiallyEmpty()
-    {
-        var context = MakeSuiteContext();
         AssertInt(context.GetStore().Count()).IsEqual(0);
     }
 
-    #endregion
-
-    #region TestCaseConstructor
-
 
     [TestCase]
-    public void TestTestConstructorInheritsTestSuiteTypeFromParent()
+    public void TestConstructorAtTestLevel()
     {
-        var suite = MakeSuiteContext();
-        var test = MakeTestContext(suite, nameof(TestTestConstructorInheritsTestSuiteTypeFromParent), []);
+        var suite = CreateSuiteContext();
+        var test = CreateTestContext(suite, nameof(TestConstructorAtTestLevel), []);
         AssertThat(test.GetTestSuiteType()).IsEqual(typeof(ExtensionContextTest));
-    }
-
-    [TestCase]
-    public void TestTestConstructorInheritsTestSuiteInstanceFromParent()
-    {
-        var suite = MakeSuiteContext();
-        var test = MakeTestContext(suite, nameof(TestTestConstructorInheritsTestSuiteInstanceFromParent), []);
         AssertThat(test.GetTestSuiteInstance()).IsEqual(SuiteNode);
-    }
-
-    [TestCase]
-    public void TestTestConstructorReturnsCorrectTestMethod()
-    {
-        var suite = MakeSuiteContext();
-        var test = MakeTestContext(suite, nameof(TestTestConstructorReturnsCorrectTestMethod), []);
-        AssertThat(test.GetTestMethod()).IsEqual(GetMethod(nameof(TestTestConstructorReturnsCorrectTestMethod)));
-    }
-
-    [TestCase]
-    public void TestTestConstructorReturnsCorrectTestCaseName()
-    {
-        var suite = MakeSuiteContext();
-        var test = MakeTestContext(suite, nameof(TestTestConstructorReturnsCorrectTestCaseName), []);
-        AssertString(test.GetTestCaseName()).IsEqual(nameof(TestTestConstructorReturnsCorrectTestCaseName));
-    }
-
-    [TestCase]
-    public void TestTestConstructorReturnsEmptyArguments()
-    {
-        var suite = MakeSuiteContext();
-        var test = MakeTestContext(suite, nameof(TestTestConstructorReturnsEmptyArguments), []);
+        AssertThat(test.GetTestMethod()).IsEqual(GetMethod(nameof(TestConstructorAtTestLevel)));
+        AssertString(test.GetTestCaseName()).IsEqual(nameof(TestConstructorAtTestLevel));
         AssertThat(test.GetTestCaseArguments()).IsNotNull();
         AssertInt(test.GetTestCaseArguments()!.Count).IsEqual(0);
-    }
-
-    [TestCase]
-    public void TestTestConstructorReturnsCorrectArguments()
-    {
-        var suite = MakeSuiteContext();
-        List<object?> args = ["hello", 42, null];
-        var test = MakeTestContext(suite, nameof(TestTestConstructorReturnsCorrectArguments), args);
-        AssertThat(test.GetTestCaseArguments()).IsNotNull();
-        AssertThat(test.GetTestCaseArguments()!).ContainsExactly("hello", 42, null);
-    }
-
-    [TestCase]
-    public void TestTestConstructorStoreIsLinkedToParentStore()
-    {
-        var suite = MakeSuiteContext();
+        
         suite.GetStore().Add("key", "suite-value");
-        var test = MakeTestContext(suite, nameof(TestTestConstructorStoreIsLinkedToParentStore), []);
         AssertString(test.GetStore().Value<string>("key")).IsEqual("suite-value");
+        
+        
+        List<object?> args = ["hello", 42, null];
+        var argsTest = CreateTestContext(suite, nameof(TestConstructorAtTestLevel), args);
+        AssertThat(argsTest.GetTestCaseArguments()).IsNotNull();
+        AssertThat(argsTest.GetTestCaseArguments()!).ContainsExactly("hello", 42, null);
     }
-
-    #endregion
-
 }
